0d8682cf4e3f2a399c5576dda5b88e6387809943
Brandon De Leon <brandondeleon.615@gmail.com>
2021-07-30 11:31:47 -0500

    added database array on database.js

--- scripts/database.js
+++ scripts/database.js
@@ -1,3 +1,31 @@
 const database ={
-    
+    PaintColors: [
+        {id: 1, color: "Silver", price: 200},
+        {id: 2, color: "Midnight Blue", price: 100},
+        {id: 3, color: "Firebrick Red", price: 600},
+        {id: 4, color: "Spring Green", price: 500}
+    ],
+    Wheels: [
+        {id:1, wheel:"17-inch Pair Radial",price: 600},
+        {id:2, wheel:"17-inch Pair Radial Black",price: 800},
+        {id:3, wheel:"18-inch Pair Spoke Silver",price: 1000},
+        {id:4, wheel:"18-inch Pair Spoke Black",price: 1000},
+    ],
+    InteriorSeats: [
+        {id: 1, interior: "Beige Fabric", price: 300},
+        {id: 2, interior: "Charcoal Fabric", price: 400},
+        {id: 3, interior: "White Leather", price: 600},
+        {id: 4, interior: "Black Leather", price: 700},
+    ],
+    Technology: [
+        {id:1, tech:"Basic Package (basic sound system)", price: 600},
+        {id:2, tech:"Navigation Package (includes integrated navigation controls)", price: 800},
+        {id:3, tech:"Visibility Package (includes side and rear cameras)", price: 1000},
+        {id:4, tech:"Ultra Package (includes navigation and visibility packages)", price: 1500},
+    ],
+    CustomCarOrders: [
+        {id: 1, paintColorId,wheelId,interiorSeatsId,technologyId          }
+    ]
+
+
 }
